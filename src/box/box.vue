d90f20157dc049f2a0eedf3f9aa005e3e200df6f
writethesky <writethesky@163.com>
2017-07-27 16:28:06 +0800

    更改对change事件的定义

--- src/box/box.vue
+++ src/box/box.vue
@@ -12,7 +12,7 @@
                     <span class="pop-up-left" :style="{width: style.itemLeftTextWidth}">{{item.name}}</span>
                     <span class="pop-and">：</span>
                     <span :style="{left: style.itemLeftWidth}" class="pop-up-input">
-                        <input v-if="!item.type" type="text" name="" v-model:value="item.value" class="pop-up-input" v-on:keyup="changed(i)" v-on:keydown="changed(i)">
+                        <input v-if="!item.type" type="text" name="" v-model:value="item.value" class="pop-up-input" v-on:blur="changed(i)">
                         <select v-if="item.type == 'select'" type="text" name="" v-model:value="item.value" class="pop-up-input" v-on:change="changed(i)" >
                             <option v-for="option in item.options" :value="option.value">{{option.text}}</option>
                         </select>
